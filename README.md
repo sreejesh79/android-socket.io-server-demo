9a834542b2b501d0485b57e928e45ea80bffbbef
sreejesh_pillai <sreejesh@kriyatma.com>
2015-06-17 10:13:28 +0530

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # android-socket.io-server-demo
+
+1) install node
+2) npm install from the root. This will install all the dependencies
+3) node index.js - this will start the server
+4) to change the port  open index.js and change it in line number 29. Right now it is 3000
---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by sreejeshpillai on 09/05/15.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html');
+})
+io.on('connection',function(socket){
+    console.log('one user connected '+socket.id);
+    socket.on('message',function(data){
+        var sockets = io.sockets.sockets;
+        sockets.forEach(function(sock){
+            if(sock.id != socket.id)
+            {
+                sock.emit('message',data);
+            }
+        })
+    })
+    socket.on('disconnect',function(){
+        console.log('one user disconnected '+socket.id);
+    })
+})
+
+
+
+http.listen(3000,function(){
+    console.log('server listening on port 3000');
+})
